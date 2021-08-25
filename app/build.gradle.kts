31b3bfe53fa0c6d3ac2d775a8bed0b5d3b378ad2
Danil Yudov <yda@telepayural.ru>
2021-08-25 15:47:14 +0500

    Add libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,23 @@ android {
 }
 
 dependencies {
+    val daggerVersion = "2.38.1"
+    val retrofitVersion = "2.9.0"
+
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    testImplementation("junit:junit:4.+")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    implementation("com.google.dagger:dagger-android:$daggerVersion")
+    kapt("com.google.dagger:dagger-android-processor:$daggerVersion")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.0")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:$retrofitVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
